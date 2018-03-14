41f9baa6b3e7bc3fca3a23bd5b79ec38c9e6332b
Steffen Kiilsholm Hansen <stef7182@edu.eal.dk>
2018-03-14 11:33:56 +0100

    Add project files.

---
+++ SortingAlgorithms/Controller.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+  public  class Controller
+    {
+        public List<int> NaiveSort(List<int> list)
+        {
+           
+            int n = list.Count;
+            for(int i = 1; i < n; i++)
+            {
+                for(int j = 0; j < n - 1; j++)
+                {
+                    if(list[j] > list[j + 1])
+                    {
+                        //swap
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+            return list;
+        }
+
+        public List<int> BubbleSort(List<int> list)
+        {
+
+            int n = list.Count;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        //swap
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+            return list;
+        }
+
+
+    }
+}
---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+
+            List<int> list = new List<int>();
+
+            for (int m = 10000; m > 0; m--)
+            {
+                list.Add(m);
+            }
+            //foreach (int i in list)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            Console.WriteLine("NaiveSort:");
+            list = controller.BubbleSort(list);
+            //foreach (int k in list)
+            //{
+            //    Console.WriteLine(k);
+            //}
+            //Console.WriteLine("BubbleSort:");
+            //list = controller.BubbleSort(list);
+            //foreach (int k in list)
+            //{
+            //    Console.WriteLine(k);
+            //}
+        }
+    }
+}
